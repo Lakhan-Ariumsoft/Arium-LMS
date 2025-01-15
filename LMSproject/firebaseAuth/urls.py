8291d700ee7c92282dacf578ccf9ecb5ce45aa9e
Lakhan Shrivastav <lakhan.shrivastav@ariumsoft.com>
2025-01-16 00:07:18 +0530

    Lakhan S | Code logic for urls and views.py

--- LMSproject/firebaseAuth/urls.py
+++ LMSproject/firebaseAuth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import SendOTP, VerifyOTP, Signup, Login
+# from .views import SendOTP, VerifyOTP, Signup, Login
 
 urlpatterns = [
-    path('getOtp/', SendOTP.as_view(), name='send-otp'),
-    path('verify-otp/', VerifyOTP.as_view(), name='verify-otp'),
-    path('signup/', Signup.as_view(), name='signup'),
-    path('login/', Login.as_view(), name='login'),
+    # path('getOtp/', SendOTP.as_view(), name='send-otp'),
+    # path('verify-otp/', VerifyOTP.as_view(), name='verify-otp'),
+    # path('signup/', Signup.as_view(), name='signup'),
+    # path('login/', Login.as_view(), name='login'),
 ]
