f2aba067dd34b32c999b4fbc4256be82cee25987
Lakhan Shrivastav <lakhan.shrivastav@ariumsoft.com>
2025-01-20 12:34:11 +0530

    Lakhan S | Urls.py commented for Auth firebase

--- LMSproject/firebaseAuth/urls.py
+++ LMSproject/firebaseAuth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import GetOTP, VerifyOTP, SignUp, Login
+# from .views import GetOTP, VerifyOTP, SignUp, Login
 
 urlpatterns = [
-    path('getOtp/', GetOTP.as_view(), name='send-otp'),
-    path('verify-otp/', VerifyOTP.as_view(), name='verify-otp'),
-    path('signup/', SignUp.as_view(), name='signup'),
-    path('login/', Login.as_view(), name='login'),
+    # path('getOtp/', GetOTP.as_view(), name='send-otp'),
+    # path('verify-otp/', VerifyOTP.as_view(), name='verify-otp'),
+    # path('signup/', SignUp.as_view(), name='signup'),
+    # path('login/', Login.as_view(), name='login'),
 ]
