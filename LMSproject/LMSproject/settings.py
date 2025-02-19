4a368335e2af43efd18e0dfb54f3321e0c30729e
Lakhan Shrivastav <lakhan.shrivastav@ariumsoft.com>
2025-02-20 00:25:29 +0530

    Lakhan S | static url issue

--- LMSproject/LMSproject/settings.py
+++ LMSproject/LMSproject/settings.py
@@ -163,8 +163,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'STATIC_URL = '/static/'  # Ensure this line is present
 import os
+STATIC_URL = '/static/'  # Ensure this line is present
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
