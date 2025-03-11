4d5d5c1acf6276d12ed017853d5cc95483cde5f7
Lakhan Shrivastav <lakhan.shrivastav@ariumsoft.com>
2025-03-11 13:58:49 +0530

    Lakhan S |models.py changes for signed url

--- LMSproject/zoomApp/models.py
+++ LMSproject/zoomApp/models.py
@@ -4,10 +4,12 @@ from courses.models import Courses  # Ensure that the Courses model is correctly
 class Recordings(models.Model):
     course = models.ManyToManyField(Courses, related_name="recordings" , null=True)
     # course = models.ForeignKey(Courses, null=True, blank=True, on_delete=models.SET_NULL)  # Allows blank course
+    filePath = models.CharField(max_length=6000)
     title = models.CharField(max_length=200)
     meeting_id = models.CharField(max_length=50)  # Meeting ID should be unique
     duration = models.IntegerField(blank=True)  # Duration is fine, but you might want to specify time units (seconds)
-    recording_url = models.URLField(blank=True)  # URL for the recording
+    recording_url = models.URLField(max_length=10000,blank=True)  # URL for the recording
+    expiration_time = models.DateTimeField(null=True, blank=True)  # Stores signed URL expiry time
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
