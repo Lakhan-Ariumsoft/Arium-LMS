0451e897ee9f9e68bb9808ffdd4dfa1839b9bb6d
Lakhan Shrivastav <lakhan.shrivastav@ariumsoft.com>
2025-03-11 14:40:35 +0530

    Lakhan S | URLS.py for recordings

--- LMSproject/zoomApp/urls.py
+++ LMSproject/zoomApp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('zoom/webhook/', views.zoom_webhook, name='zoom_webhook'),
     path('recordings/', RecordingsView.as_view(), name='list_create_meeting'),
     path('recordings/<int:pk>/', RecordingsView.as_view(), name='retrieve_update_delete_meeting'),
-    path('GetValidRecordingUrl/', GetValidRecordingUrl.as_view(), name='signedUrl'),
+    path('GetValidRecordingUrl/<int:pk>/', GetValidRecordingUrl.as_view(), name='signedUrl'),
 ]
 
